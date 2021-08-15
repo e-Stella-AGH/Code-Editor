fdfeacd35bf8907abdade41185e8f3ba9dce7f00
Dawid Dębowski <dawid199960@gmail.com>
2021-08-15 17:18:33 +0200

    initial

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'e-stella-code-editor'
+import { CodeEditor } from 'e-stella-code-editor'
 import 'e-stella-code-editor/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <CodeEditor text="Create React Library Example 😄" />
 }
 
 export default App
